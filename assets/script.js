07e4804d5b51f2ec203546b4a80f25172f3e0a42
Carlos Magno Dresch <dresh47@gmail.com>
2020-10-25 12:02:09 -0300

    primeiro commit

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// function abrir(){
+
+//   var mostrarMenu = document.getElementById('showNav');
+     
+
+//   if (mostrarMenu.style.display == 'none') {
+//        mostrarMenu.style.display = 'block';
+//   }else{
+//        mostrarMenu.style.display = 'none';
+//   }
+
+ 
+   
+// }
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
+    <title>Pretinho - Minha História de Superaçao</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="assets/script.js"></script>
+
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <div class="logo">
+                <div class="logoimg">
+                    <a href="#"><img src="assets/images/logoPretinho.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="menu">
+                <img class="menu-opener" src="assets/images/menu.png" width="30px"  height="30px" onclick="abrir()" >
+                <nav id="showNav">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="https://api.whatsapp.com/send?phone=+5527996926624" target="_blank">Meus Livros</a></li>
+
+
+
+                        <li>
+                            <a href="https://api.whatsapp.com/send?phone=+5527996926624" target="_blank">
+                                <img src="assets/images/whatsapp.png" border="0" width="30" height="30">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/walmir.coelho.37" target="_blank">
+                                <img src="assets/images/face.png" border="0" width="30" height="30">
+                            </a>
+                        </li>
+                        <li> <a href="https://www.instagram.com/walmir_pretinho/">
+                                <img src="assets/images/insta.png" target="_blank" border="0" width="30" height="30">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCjEELJpfZYSFJ26sUewJLwA" target="_blank">
+                                <img src="assets/images/youtube.png" border="0" width="30" height="30">
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                        
+            </div>
+                         
+        </div>
+    </header>
+    <main>
+        <section class="banner">
+            
+                    <div class="slidearea">
+                        <p>Prepare-se para uma História de Superação<p>
+
+                    </div>
+                
+
+        </section>
+
+        <section class="default light">
+
+            <div class="section-title">Minha História</div>
+            <div class="section-desc">Quem sou</div>
+            <div class="section-body">
+                <div class="section-aboutus">
+                    <div class="section-aboutus--left">
+
+                        <p>Em uma pequena cidade de Minas Gerais, Serra dos Aimorés, onde habitavam os índios cujo nome
+                            da tribo deu nome à cidade, na manhã de um domingo chuvoso, nasceu uma criança saudável. Era
+                            mais uma bênção de 14 filhos de Davi e Domingas. Com nove meses já andava. A vida era dura
+                            na roça, muito trabalho e pouca recompensa. No ano de 1966, quando completei um ano de vida,
+                            fui acometido de uma febre muito alta que veio com vomitos, diarréia e espasmos, quando
+                            infelizmente fui diagnosticado com paralisia infantil (Poliomielite). Foi um caos e uma
+                            tristeza muito grande na família e na vizinhança.
+                            Daí em diante não levantei mais da cama e começou o processo de atrofiação dos membros
+                            inferiores e superiores. Ficava todo enroladinho e como não tinha uma cama decente, ficava o
+                            tempo todo em uma esteira de palha feita pela minha saudosa mãe. Minha tia Maria sempre
+                            amorosa me cuidando e providenciando tudo para que tivesse o mínimo de conforto mesmo na
+                            esteira de palha...</p>
+
+                        <h1 style="margin-top:30px;font-size:20px">Se voce quer saber o desfecho dessa incrível história de vida,
+                            entre em contato para adquirir essa história <a href="https://api.whatsapp.com/send?phone=+5527996926624" target="_blank">AQUI</a>
+                        </h1>
+
+                    </div>
+                    <div class="section-aboutus--right">
+                        <a href="https://api.whatsapp.com/send?phone=+5527996926624" target="_blank"><img src="assets/images/banner2.jpeg" alt=""> </a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+
+
+
+
+        <section class="default light bg-resources">
+            <div class="section-resources--area">
+
+                <div class="section-title">Livros</div>
+                <div class="section-desc">Adquira aqui meus livros</div>
+                <div class="section-body">
+                    <div class="section-resources">
+
+                        <div class="section-resourcer--books">
+                            <a href="https://api.whatsapp.com/send?phone=+5527996926624" target="_blank"><img src="assets/images/livroNovo.jpg" alt=""></a>
+                        </div>
+                        <div class="section-resourcer--books">
+                            <a href="https://api.whatsapp.com/send?phone=+5527996926624" target="_blank"><img src="assets/images/livroAntigo.jpg" alt=""></a>
+                        </div>
+
+
+
+
+                    </div>
+                    <div class="section-linha"></div>
+
+
+                </div>
+            </div>
+
+        </section>
+
+
+
+    </main>
+    <footer>
+
+        <div class="section-footer--social-corporyt" >
+            pretinhoolivro.com.br @2020
+        </div>
+
+    </footer>
+
+
+    
+    
+</body>
+
+</html>
